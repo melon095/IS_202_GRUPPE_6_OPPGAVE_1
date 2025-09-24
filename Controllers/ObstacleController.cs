4c7b65a1aa9d00a16ce19bc18245be6f5b815519
Moussa Farhat <farhatmoussa4@gmail.com>
2025-09-24 12:23:59 +0200

    Oppgaven2x

---
+++ Controllers/ObstacleController.cs
@@ -0,0 +1,30 @@
+﻿using Gruppe6Oppgave1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Gruppe6Oppgave1.Controllers
+{
+    public class ObstacleController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        [HttpGet]
+        public ActionResult DataForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DataForm(ObstacleData obstacledata)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(obstacledata);
+            }
+            return View("Overview", obstacledata);
+        }
+
+    }
+}
