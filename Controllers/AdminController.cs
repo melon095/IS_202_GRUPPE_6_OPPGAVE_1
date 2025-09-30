523d8c071435ca9550e99040f32625fd82193449
Bruker <emvisic@gmail.com>
2025-09-30 13:00:46 +0200

    Kommentarer

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -5,21 +5,17 @@ using static Gruppe6Oppgave1.Web.Models.GetAllReportsModel;
 
 namespace Gruppe6Oppgave1.Web.Controllers
 {
-    // Klassen AdminController arver fra Controller klassen fra ASP NET
+    // AdminController endrer ikke på data, den viser bare rapport tabellen
     public class AdminController : Controller
     {
-        // Her er det kun tilgang til _context innenfor klassen DatabaseContext
         private readonly DatabaseContext _context;
-        // Konstruktør
         public AdminController(DatabaseContext context)
         {
             _context = context;
         }
-        // IActionResult forteller hva som skal vises i view
+        // IActionResult lager liste som View returnerer
         public IActionResult Index()
         {
-            // Variabel reports blir hentet fra Marker listen
-            // Variabel Model tar inn listen fra GetAllReportsModel
             var reports = _context.Marker.ToList();
             var Model = new GetAllReportsModel();
             // Foreach løkke som legger til hver rad i Model
@@ -34,7 +30,6 @@ namespace Gruppe6Oppgave1.Web.Controllers
                     Lattitude = report.Lattitude
                 });
             }
-            // Returnerer view Model
             return View(Model);
         }
     }
