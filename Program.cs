da2a24fa6fcd0d9e64957e664f7d83fe6fb96f92
Joachim Flottorp <joachim@flottorp.org>
2025-10-04 17:40:39 +0200

    Legg til docker-compose og automatisk migrer database på oppstart

--- Program.cs
+++ Program.cs
@@ -26,6 +26,12 @@ builder.Services.AddDbContext<DatabaseContext>(options =>
 
 var app = builder.Build();
 
+using(var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
